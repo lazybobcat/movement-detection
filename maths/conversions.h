114484a737f82d24ac5109d4f49ce4f2724333bb
Boutter Loic <loic.boutter@gmail.com>
2013-01-07 09:23:09 +0100

    Conversion header completed

--- maths/conversions.h
+++ maths/conversions.h
@@ -10,13 +10,47 @@ namespace carto
         // Fonctions de convertion pour l'image RGB
         namespace ppm
         {
+            extern float fx;    // Distance focale de la caméra à l'image
+            extern float fy;    // Distance focale de la caméra à l'image
+            extern float cu;    // Centre optique de la caméra = projection du centre de la caméra dans l'image
+            extern float cv;    // Centre optique de la caméra
+
+            /**
+             * @brief Converts the coordinates of a local pixel in world coordinates. The depth we take is 1.
+             * @param pixel The pixel position to convert
+             * @return A vector with world coordinates (the depth [z] coordinates is 1)
+             */
             Vector3f toRGBCameraCoordinates(const Vector2u& pixel);
+
+            /**
+             * @brief Converts a world coordinate into a local (image) coordinate.
+             * @param position The world coordinates
+             * @return The coordinates in pixels (relative to the image)
+             */
+            Vector2u toRGBImageCoordinates(const Vector3f& position);
         }
 
         // Fonctions de convertion pour l'image de profondeur
         namespace pgm
         {
+            extern float fx;    // Distance focale de la caméra à l'image
+            extern float fy;    // Distance focale de la caméra à l'image
+            extern float cu;    // Centre optique de la caméra = projection du centre de la caméra dans l'image
+            extern float cv;    // Centre optique de la caméra
+
+            /**
+             * @brief Converts the coordinates of a local pixel in world coordinates. The depth we take is 1.
+             * @param pixel The pixel position to convert
+             * @return A vector with world coordinates (the depth [z] coordinates is 1)
+             */
             Vector3f toRGBCameraCoordinates(const Vector2u& pixel);
+
+            /**
+             * @brief Converts a world coordinate into a local (image) coordinate.
+             * @param position The world coordinates
+             * @return The coordinates in pixels (relative to the image)
+             */
+            Vector2u toDepthImageCoordinates(const Vector3f& position);
         }
     }
 }
