c5fd7d4d0993c6ff9f418a624343edd2ab4c7308
Boutter Loic <loic.boutter@gmail.com>
2013-01-07 09:47:31 +0100

    English is better

--- maths/conversions.h
+++ maths/conversions.h
@@ -7,18 +7,18 @@ namespace carto
 {
     namespace maths
     {
-        // Fonctions de convertion pour l'image RGB
+        // Conversion functions for RGB camera
         namespace ppm
         {
-            extern float fx;    // Distance focale de la caméra à l'image
-            extern float fy;    // Distance focale de la caméra à l'image
-            extern float cu;    // Centre optique de la caméra = projection du centre de la caméra dans l'image
-            extern float cv;    // Centre optique de la caméra
+            extern float fx;    // Focal Distance from camera to image
+            extern float fy;    // Focal Distance from camera to image
+            extern float cu;    // Camera Optical Center = projection of the camera center on the image
+            extern float cv;    // Camera Optical Center
 
             /**
-             * @brief Converts the coordinates of a local pixel in world coordinates. The depth we take is 1.
+             * @brief Converts the coordinates of a local pixel in world coordinates.
              * @param pixel The pixel position to convert
-             * @param depth The pixel depth in meters
+             * @param depth The pixel depth in meters (found with depth camera)
              * @return A vector with world coordinates (the depth [z] coordinates is 1)
              */
             Vector3f toRGBCameraCoordinates(const Vector2u& pixel, const float depth);
@@ -31,18 +31,18 @@ namespace carto
             Vector2u toRGBImageCoordinates(const Vector3f& position);
         }
 
-        // Fonctions de convertion pour l'image de profondeur
+        // Conversion functions for depth camera
         namespace pgm
         {
-            extern float fx;    // Distance focale de la caméra à l'image
-            extern float fy;    // Distance focale de la caméra à l'image
-            extern float cu;    // Centre optique de la caméra = projection du centre de la caméra dans l'image
-            extern float cv;    // Centre optique de la caméra
+            extern float fx;    // Focal Distance from camera to image
+            extern float fy;    // Focal Distance from camera to image
+            extern float cu;    // Camera Optical Center = projection of the camera center on the image
+            extern float cv;    // Camera Optical Center
 
             /**
-             * @brief Converts the coordinates of a local pixel in world coordinates. The depth we take is 1.
+             * @brief Converts the coordinates of a local pixel in world coordinates.
              * @param pixel The pixel position to convert
-             * @param depth The pixel depth in meters
+             * @param depth The pixel depth in meters (found with depth camera)
              * @return A vector with world coordinates (the depth [z] coordinates is 1)
              */
             Vector3f toRGBCameraCoordinates(const Vector2u& pixel, const float depth);
